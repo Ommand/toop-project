9f6dd7aadbdbd0df021177e76d90d2f218602bc3
Stepdan <stepdan94@gmail.com>
2016-02-14 22:17:39 +0600

    Новые интерфейсы матрицы и вектора

--- toop-project/toop-project/src/Matrix/IMatrix.cs
+++ toop-project/toop-project/src/Matrix/IMatrix.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using toop_project.src.Vector;
 
 namespace toop_project.src.Matrix
 {
     interface IMatrix
     {
-        bool InitializeFrom(IMatrix matr);
+        IVector Multiply(IVector x);// A*x
+        IVector TMultiply(IVector x);// At*x
 
-        //public delegate void ProcessElement(int i, int j, double d);
-       // void Run(ProcessElement fun);//Пробежаться по всем ненулевым элементам матрицы
+        IVector LMult(IVector x, bool UseDiagonal);//L*x , Если false, то диагональ нулевая
+        IVector UMult(IVector x, bool UseDiagonal);//U*x, Если false, то диагональ нулевая
 
-        //IVector Multiply(IVector x);
-        //IVector TMultiply(IVector x);
+        IVector LSolve(IVector x);//L-1*x
+        IVector USolve(IVector x);//U-1*x
 
-        //IVector LMult(IVector x,bool UseDiagonal);//L*x , Если false, то диагональ нулевая
-        //IVector UMult(IVector x, bool UseDiagonal);//U*x, Если false, то диагональ нулевая
+        IVector LtSolve(IVector x);//Lt-1*x
+        IVector UtSolve(IVector x);//Ut-1*x
 
-        //IVector LSolve(IVector x, bool UseDiagonal);//L-1*x, Если false, то диагональ единички
-        //IVector USolve(IVector x, bool UseDiagonal);//U-1*x, Если false, то диагональ единички
-
-        //IVector LtSolve(IVector x, bool UseDiagonal);//Lt-1*x Если false, то диагональ единички
-        //IVector UtSolve(IVector x, bool UseDiagonal);//Ut-1*x Если false, то диагональ единички
-        //IVector Diagonal { get; }
+        IVector Diagonal { get; }
         int Size { get; }
     }
 }
