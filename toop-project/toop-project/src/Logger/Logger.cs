0154183eff7073d17418da8a321d71abe1705297
Алексей <Lexxxxx731@gmail.com>
2016-02-15 23:54:26 +0600

    Собрал класс Logger

---
+++ toop-project/toop-project/src/Logger/Logger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toop_project.src.Logger
+{
+    class Logger
+    {
+        private enum MessageType
+        {
+            ERROR,
+            DEBUG,
+            INFO
+        };
+
+        private string defaultLogFile = "log/log.txt";
+        private System.IO.StreamWriter fileStream;
+        private List<string> log = new List<string>(); 
+        private static Logger instance = new Logger();
+        private Logger()
+        {
+            try
+            {
+                fileStream = new System.IO.StreamWriter(defaultLogFile, true);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(String.Format("Logger open file exception: {0}", e));
+            }
+
+        }
+        private void AddLine(MessageType type,string line)
+        {
+            string message = String.Format("[{0}][{1}] {2}", type, DateTime.Now, line);
+            log.Add(message);
+            fileStream.WriteLine(message);
+        }
+        public static Logger Instance()
+        {
+            return instance;
+        }
+
+        public void SetFile(string filename)
+        {
+            fileStream.Close();
+            fileStream = new System.IO.StreamWriter(filename);
+        }
+
+        public void Info(string message)
+        {
+            AddLine(MessageType.INFO, message);
+        }
+
+        public void Debug(string message)
+        {
+            AddLine(MessageType.DEBUG, message);
+        }
+
+        public void Error(string message)
+        {
+            AddLine(MessageType.ERROR, message);
+        }
+
+        public List<string> Log
+        {
+            get { return log; }
+        }
+
+    }
+}
--- toop-project/toop-project/src/Vector/IVector.cs
+++ toop-project/toop-project/src/Vector/IVector.cs
@@ -16,4 +16,91 @@ namespace toop_project.src.Vector
         void Nullify();// Занулить вектор
         int Size { get; }
     }
-}
+
+    
+    
+        class Vector : IVector
+        {
+            private IList<double> _vector;
+
+            public Vector()
+            {
+                _vector = new List<double>();
+            }
+
+            public Vector(int capacity)
+            {
+                _vector = new List<double>(capacity);
+            }
+
+            public void Add(double value)
+            {
+                _vector.Add(value);
+            }
+
+            #region IVector
+
+            public double this[int i]
+            {
+                get
+                {
+                    return _vector[i];
+                }
+
+                set
+                {
+                    _vector[i] = value;
+                }
+            }
+
+            public int Size
+            {
+                get
+                {
+                    return _vector.Count;
+                }
+            }
+
+            public IVector ConstMult(double multiple)
+            {
+                Vector vector = new Vector();
+                foreach (var value in _vector)
+                {
+                    vector.Add(multiple * value);
+                }
+                return vector;
+            }
+
+            public double Norm()
+            {
+                return Math.Sqrt(_vector.Select(value => value * value).Sum());
+            }
+
+            public void Nullify()
+            {
+                _vector.Clear();
+            }
+
+            public double ScalarMult(IVector vec)
+            {
+                double sum = 0;
+                for (int index = 0; index < vec.Size; index++)
+                {
+                    sum += _vector[index] * vec[index];
+                }
+                return sum;
+            }
+
+            public IVector SumVectors(IVector vec)
+            {
+                Vector vector = new Vector();
+                for (int index = 0; index < vec.Size; index++)
+                {
+                    vector[index] = _vector[index] + vec[index];
+                }
+                return vector;
+            }
+
+            #endregion IVector
+        }
+    }
