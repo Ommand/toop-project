d1d8f0adfdaaff131ee28141512ed0c285b7dd7b
Stepdan <xJhetonx@yandex.ru>
2016-02-16 16:53:29 +0600

    Убраны интерфейсы матрицы и вектора, создан асбтрактный класс матрицы, класс разреженной матрицы и вектора

---
+++ toop-project/toop-project/src/Vector_/Vector.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toop_project.src.Vector_
+{
+    public class Vector
+    {
+        private IList<double> _vector;
+
+        public Vector()
+        {
+            _vector = new List<double>();
+        }
+
+        public Vector(int capacity)
+        {
+            _vector = new List<double>(capacity);
+        }
+
+        public void Add(double value)
+        {
+            _vector.Add(value);
+        }
+
+        #region Vector
+
+        public double this[int i]
+        {
+            get
+            {
+                return _vector[i];
+            }
+
+            set
+            {
+                _vector[i] = value;
+            }
+        }
+
+        public int Size
+        {
+            get
+            {
+                return _vector.Count;
+            }
+        }
+
+        public static Vector operator *(Vector v, double multiple)
+        {
+            Vector vector = new Vector(v.Size);
+            for (int index = 0; index < v.Size; index++)
+            {
+                vector[index] = multiple * v[index];
+            }
+            return vector;
+        }
+
+        public double Norm()
+        {
+            return Math.Sqrt(_vector.Select(value => value * value).Sum());
+        }
+
+        public void Nullify()
+        {
+            for (int index = 0; index < _vector.Count; index++)
+            {
+                _vector[index] = 0;
+            }
+        }
+
+        public static double operator *(Vector v1, Vector v2)
+        {
+            double sum = 0;
+            if (v1.Size == v2.Size)
+            {
+                for (int index = 0; index < v1.Size; index++)
+                {
+                    sum += v1[index] * v2[index];
+                }
+                return sum;
+            }
+            else
+                throw new Exception("Не совпадение длин у операндов при скалярном умножении");// Разобраться с типом исключения
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            if (v1.Size == v2.Size)
+            {
+                Vector vector = new Vector(v1.Size);
+                for (int index = 0; index < v1.Size; index++)
+                {
+                    vector[index] = v1[index] + v2[index];
+                }
+                return vector;
+            }
+            else
+                throw new Exception("Не совпадение длин у операндов при сложении");
+        }
+        #endregion Vector
+    }
+}
