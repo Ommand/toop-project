1667c4c348cb7e43783a7eb74d9a92d99a82df5d
Fenita <xJhetonx@yandex.ru>
2016-02-17 08:27:37 +0600

    Умножение нижнего треугольника на вектор

--- toop-project/toop-project/src/Matrix/SparseMatrix.cs
+++ toop-project/toop-project/src/Matrix/SparseMatrix.cs
@@ -44,7 +44,26 @@ namespace toop_project.src.Matrix
 
         public override Vector LMult(Vector x, bool UseDiagonal)
         {
-            throw new NotImplementedException();
+            int i, j;
+            Vector v = new Vector(n);
+            if (UseDiagonal == true)//  если умножение с диагональю 
+            {
+                for (i = 0; i < n; i++)
+                {
+                    v[i] = di[i] * x[i];
+                    for (j = ia[i]; j < ja[i + 1]; j++)                    
+                        v[i] += al[j] * x[ja[j]];             
+                }
+                return v;
+            }
+            else// если без диагонали
+            {
+                for (i = 0; i < n; i++)              
+                    for (j = ia[i]; j < ja[i + 1]; j++)
+                        v[i] += al[j] * x[ja[j]];
+                
+                return v;
+            }
         }
 
         public override Vector LSolve(Vector x, bool UseDiagonal)
