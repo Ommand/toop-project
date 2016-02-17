afaad3d8e28e37a30bd7b45f73dbccffe333dda9
KurskijDenis <kurskij.denis@gmail.com>
2016-02-17 22:31:23 +0300

    Изменил интерфейсgit add IPreconditione!

--- toop-project/toop-project/src/Preconditioner/IPreconditioner.cs
+++ toop-project/toop-project/src/Preconditioner/IPreconditioner.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using toop_project.src.Matrix;
     namespace toop_project.src.Preconditioner
     {
         interface IPreconditioner
         {
-            void LU(toop_project.src.Matrix.IMatrix matrix, out toop_project.src.Matrix.IMatrix matrixPrecond);
+        BaseMatrix LU();
+        BaseMatrix LLt();
+        BaseMatrix LUsq();
             // void LLT (IMatrix matrix, out IMatrix matrixPrecond);
         }
     }
