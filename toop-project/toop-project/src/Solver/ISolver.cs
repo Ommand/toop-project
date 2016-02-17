82939444433a65d33d977ccb1f9a52fc1f517ed0
Gidroponik54 <kliminigor@gmail.com>
2016-02-17 14:34:38 +0600

    Added using and changed types input and output data

--- toop-project/toop-project/src/Solver/ISolver.cs
+++ toop-project/toop-project/src/Solver/ISolver.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using toop_project.src.Matrix;
+using toop_project.src.Vector_;
+using toop_project.src.Logger;
 
 namespace toop_project.src.Solver
 {
     interface ISolver
     {
-   IVector Solve(IMatrix matr, IVector rightpart, IVector initial,
-                        IIterationLogger logger, double eps, int maxiter);
+   Vector Solve(BaseMatrix matr, Vector rightpart, Vector initial,
+                        ILogger logger, double eps, int maxiter);
         string Name { get; }
     }
 }
