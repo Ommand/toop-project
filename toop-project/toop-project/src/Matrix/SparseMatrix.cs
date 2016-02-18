3d919e72060c613af5426fbd8fdeea17b8bf7576
Fenita <xJhetonx@yandex.ru>
2016-02-18 09:47:45 +0600

    Немного поправил разреженную матрицы ( простите, что не в отдельной задаче )

--- toop-project/toop-project/src/Matrix/SparseMatrix.cs
+++ toop-project/toop-project/src/Matrix/SparseMatrix.cs
@@ -24,6 +24,8 @@ namespace toop_project.src.Matrix
             this.di = di;
             n = di.Count();
         }
+        #region Matrix
+
         public override Vector Diagonal
         {
             get
@@ -45,239 +47,292 @@ namespace toop_project.src.Matrix
 
         public override Vector LMult(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)//  если умножение с диагональю 
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов LMult");
+            else
             {
-                for (i = 0; i < n; i++)
+                int i, j;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)//  если умножение с диагональю 
                 {
-                    v[i] = di[i] * x[i];
-                    for (j = ia[i]; j < ja[i + 1]; j++)                    
-                        v[i] += al[j] * x[ja[j]];             
+                    for (i = 0; i < n; i++)
+                    {
+                        v[i] = di[i] * x[i];
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[i] += al[j] * x[ja[j]];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// если без диагонали
-            {
-                for (i = 0; i < n; i++)              
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[i] += al[j] * x[ja[j]];
-                
-                return v;
+                else// если без диагонали
+                {
+                    for (i = 0; i < n; i++)
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[i] += al[j] * x[ja[j]];
+
+                    return v;
+                }
             }
         }
 
         public override Vector LSolve(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            double result;
-            Vector v = new Vector(n);
-            if(UseDiagonal == true)// Деление на динагональ
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов LSolve");
+            else
             {
-                for (i = 0; i < n; i++)
+                int i, j;
+                double result;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)// Деление на динагональ
                 {
-                    result = 0;
-                    for ( j = ia[i]; j < ia[i + 1]; j++)                    
-                        result += al[j] * v[ja[j]];		            
-		            v[i] = (x[i] - result) / di[i];
-	            }
-                return v;
-            }
-            else// Без деления на диагональ
-            {
-                for (i = 0; i < n; i++)
-                {
-                    result = 0;
-                    for (j = ia[i]; j < ia[i + 1]; j++)
-                        result += al[j] * v[ja[j]];
-                    v[i] = (x[i] - result);
+                    for (i = 0; i < n; i++)
+                    {
+                        result = 0;
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            result += al[j] * v[ja[j]];
+                        v[i] = (x[i] - result) / di[i];
+                    }
+                    return v;
                 }
-                return v;
+                else// Без деления на диагональ
+                {
+                    for (i = 0; i < n; i++)
+                    {
+                        result = 0;
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            result += al[j] * v[ja[j]];
+                        v[i] = (x[i] - result);
+                    }
+                    return v;
+                }
             }
         }
 
         public override Vector LtMult(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)//  если умножение с диагональю 
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при LtMult");
+            else
             {
-                for (i = 0; i < n; i++)
+                int i, j;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)//  если умножение с диагональю 
                 {
-                    v[i] = di[i] * x[i];
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[ja[j]] += al[j] * x[i];
+                    for (i = 0; i < n; i++)
+                    {
+                        v[i] = di[i] * x[i];
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[ja[j]] += al[j] * x[i];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// если без диагонали
-            {
-                for (i = 0; i < n; i++)
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[ja[j]] += al[j] * x[i];
+                else// если без диагонали
+                {
+                    for (i = 0; i < n; i++)
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[ja[j]] += al[j] * x[i];
 
-                return v;
+                    return v;
+                }
             }
         }
 
         public override Vector LtSolve(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)// Деление на динагональ
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при LtSolve");
+            else
             {
-                v = x;// в смысле копирование элементов
-                for (i = n - 1; i >= 0; i--)
+                int i, j;
+                Vector v;
+                if (UseDiagonal == true)// Деление на динагональ
                 {
-                    v[i] /= di[i];
-                    for (j = ia[i]; j < ia[i + 1]; j++)
-                        v[ja[j]] -= al[j] * v[i];
+                    v = (Vector)x.Clone();// в смысле копирование элементов
+                    for (i = n - 1; i >= 0; i--)
+                    {
+                        v[i] /= di[i];
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            v[ja[j]] -= al[j] * v[i];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// Без деления на диагональ
-            {
-                v = x;// в смысле копирование элементов
-                for (i = n - 1; i >= 0; i--)
-                    for (j = ia[i]; j < ia[i + 1]; j++)
-                        v[ja[j]] -= al[j] * v[i];
-                return v;
+                else// Без деления на диагональ
+                {
+                    v = (Vector)x.Clone();// в смысле копирование элементов
+                    for (i = n - 1; i >= 0; i--)
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            v[ja[j]] -= al[j] * v[i];
+                    return v;
+                }
             }
         }
 
         public override Vector TMultiply(Vector x)
         {
-            int i, j;
-            Vector v = new Vector(n);
-            for (i = 0; i < n; i++)
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при TMultiply");
+            else
             {
-                v[i] = di[i] * x[i];
-                for (j = ia[i]; j < ja[i + 1]; j++)
-                {
-                    v[i] += au[j] * x[ja[j]];
-                    v[ja[j]] += al[j] * x[i];
-                }
-            }
-            return v;
-        }
-
-        public override Vector UMult(Vector x, bool UseDiagonal)
-        {
-            int i, j;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)//  если умножение с диагональю 
-            {
+                int i, j;
+                Vector v = new Vector(n);
                 for (i = 0; i < n; i++)
                 {
                     v[i] = di[i] * x[i];
                     for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[ja[j]] += au[j] * x[i];
+                    {
+                        v[i] += au[j] * x[ja[j]];
+                        v[ja[j]] += al[j] * x[i];
+                    }
                 }
                 return v;
             }
-            else// если без диагонали
-            {
-                for (i = 0; i < n; i++)
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[ja[j]] += au[j] * x[i];
+        }
 
-                return v;
+        public override Vector UMult(Vector x, bool UseDiagonal)
+        {
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при UMult");
+            else
+            {
+                int i, j;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)//  если умножение с диагональю 
+                {
+                    for (i = 0; i < n; i++)
+                    {
+                        v[i] = di[i] * x[i];
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[ja[j]] += au[j] * x[i];
+                    }
+                    return v;
+                }
+                else// если без диагонали
+                {
+                    for (i = 0; i < n; i++)
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[ja[j]] += au[j] * x[i];
+
+                    return v;
+                }
             }
         }
 
         public override Vector USolve(Vector x, bool UseDiagonal)
         {
-            int i, j;           
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)// Деление на динагональ
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при USolve");
+            else
             {
-                v = x;// в смысле копирование элементов
-                for (i = n - 1; i >= 0; i--)
+                int i, j;
+                Vector v;
+                if (UseDiagonal == true)// Деление на динагональ
                 {
-                    v[i] /= di[i];
-                    for (j = ia[i]; j < ia[i + 1]; j++)                    
-                        v[ja[j]] -= au[j] * v[i];                   
+                    v = (Vector)x.Clone();// в смысле копирование элементов
+                    for (i = n - 1; i >= 0; i--)
+                    {
+                        v[i] /= di[i];
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            v[ja[j]] -= au[j] * v[i];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// Без деления на диагональ
-            {
-                v = x;// в смысле копирование элементов
-                for (i = n - 1; i >= 0; i--)               
-                    for (j = ia[i]; j < ia[i + 1]; j++)                   
-			            v[ja[j]] -= au[j] * v[i];                                
-                return v;
+                else// Без деления на диагональ
+                {
+                    v = (Vector)x.Clone();// в смысле копирование элементов
+                    for (i = n - 1; i >= 0; i--)
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            v[ja[j]] -= au[j] * v[i];
+                    return v;
+                }
             }
         }
 
         public override Vector UtMult(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)//  если умножение с диагональю 
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при UtMult");
+            else
             {
-                for (i = 0; i < n; i++)
+                int i, j;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)//  если умножение с диагональю 
                 {
-                    v[i] = di[i] * x[i];
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[i] += au[j] * x[ja[j]];
+                    for (i = 0; i < n; i++)
+                    {
+                        v[i] = di[i] * x[i];
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[i] += au[j] * x[ja[j]];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// если без диагонали
-            {
-                for (i = 0; i < n; i++)
-                    for (j = ia[i]; j < ja[i + 1]; j++)
-                        v[i] += au[j] * x[ja[j]];
+                else// если без диагонали
+                {
+                    for (i = 0; i < n; i++)
+                        for (j = ia[i]; j < ja[i + 1]; j++)
+                            v[i] += au[j] * x[ja[j]];
 
-                return v;
+                    return v;
+                }
             }
         }
 
         public override Vector UtSolve(Vector x, bool UseDiagonal)
         {
-            int i, j;
-            double result;
-            Vector v = new Vector(n);
-            if (UseDiagonal == true)// Деление на динагональ
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при UtSolve");
+            else
             {
-                for (i = 0; i < n; i++)
+                int i, j;
+                double result;
+                Vector v = new Vector(n);
+                if (UseDiagonal == true)// Деление на динагональ
                 {
-                    result = 0;
-                    for (j = ia[i]; j < ia[i + 1]; j++)
-                        result += au[j] * v[ja[j]];
-                    v[i] = (x[i] - result) / di[i];
+                    for (i = 0; i < n; i++)
+                    {
+                        result = 0;
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            result += au[j] * v[ja[j]];
+                        v[i] = (x[i] - result) / di[i];
+                    }
+                    return v;
                 }
-                return v;
-            }
-            else// Без деления на диагональ
-            {
-                for (i = 0; i < n; i++)
+                else// Без деления на диагональ
                 {
-                    result = 0;
-                    for (j = ia[i]; j < ia[i + 1]; j++)
-                        result += au[j] * v[ja[j]];
-                    v[i] = (x[i] - result);
+                    for (i = 0; i < n; i++)
+                    {
+                        result = 0;
+                        for (j = ia[i]; j < ia[i + 1]; j++)
+                            result += au[j] * v[ja[j]];
+                        v[i] = (x[i] - result);
+                    }
+                    return v;
                 }
-                return v;
             }
         }
 
         public override Vector Multiply(Vector x)
         {
-            int i, j;
-            Vector v = new Vector(n);            
-            for (i = 0; i < n; i++)
+
+            if (x.Size != n)
+                throw new Exception("Несовпадение длин у операндов при Multiply");
+            else
             {
-                v[i] = di[i] * x[i];
-		        for (j = ia[i]; j < ja[i + 1]; j++)
-		            {
-			            v[i] += al[j] * x[ja[j]];
-			            v[ja[j]] += au[j] * x[i];
-		            }
+                int i, j;
+                Vector v = new Vector(n);
+                for (i = 0; i < n; i++)
+                {
+                    v[i] = di[i] * x[i];
+                    for (j = ia[i]; j < ja[i + 1]; j++)
+                    {
+                        v[i] += al[j] * x[ja[j]];
+                        v[ja[j]] += au[j] * x[i];
+                    }
+                }
+                return v;
             }
-            return v;	             
-         }
+        }
+        #endregion Matrix
+
         #region Preconditioner
         public BaseMatrix LU()
         {
