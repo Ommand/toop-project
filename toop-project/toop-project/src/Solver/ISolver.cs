92c65a47e7cd06a2bbe50e9b73de364cc7df9243
ZheleznovaAlyona <zheleznova_alyona1994@mail.ru>
2016-02-18 15:02:33 +0600

    Якоби со старым логгером

--- toop-project/toop-project/src/Solver/ISolver.cs
+++ toop-project/toop-project/src/Solver/ISolver.cs
@@ -11,8 +11,77 @@ namespace toop_project.src.Solver
 {
     interface ISolver
     {
-   Vector Solve(BaseMatrix matr, Vector rightpart, Vector initial,
-                        Logger logger, double eps, int maxiter);
+   Vector Solve(BaseMatrix matrix, Vector rightPart, Vector initialSolution,
+                        Logger logger, double epsilon, int maxIterations, double optionalRelaxationParameter = 1);
         string Name { get; }
+    }
+
+    public class Jacobi : ISolver
+    {
+
+       Vector ISolver.Solve(BaseMatrix matrix, Vector rightPart, Vector initialSolution,
+                                    Logger logger, double epsilon, int maxIterations, double optionalRelaxationParameter)                    
+        {
+            Vector x, r, diagonal;
+            int size, k;
+            double relativeResidual, w;
+            Vector DUx, DFx, Db;
+            string info;
+
+            size = initialSolution.Size;           
+            x = new Vector(size);
+            r = new Vector(size);
+            diagonal = new Vector(size);
+
+            k = 0;
+            x = initialSolution;
+            w = optionalRelaxationParameter;
+            diagonal = matrix.Diagonal;
+            r = matrix.Multiply(x) + rightPart * (-1);
+            relativeResidual = r.Norm() / rightPart.Norm();
+            Db = diagonalSolve(diagonal, rightPart);
+
+            info = "k = " + k.ToString() + "    r = " + 
+                    relativeResidual.ToString() + "\n";
+            logger.Info(info);
+            
+            while (k <= maxIterations && relativeResidual > epsilon)
+            {
+                k++;
+
+                DUx = diagonalSolve(diagonal, matrix.UMult(x, false));
+                DFx = diagonalSolve(diagonal, matrix.LMult(x, false));
+                
+                x = (DUx + DFx + Db) * w + x * (1 - w);
+
+                r = matrix.Multiply(x) + rightPart * (-1);
+                relativeResidual = r.Norm() / rightPart.Norm();
+
+                info = "k = " + k.ToString() + "    r = " +
+                        relativeResidual.ToString() + "\n";
+                logger.Info(info);
+            }
+
+            return x;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Jacobi";
+            }
+        }
+
+        Vector diagonalSolve(Vector diagonal, Vector vector)
+        {
+            int size = diagonal.Size;
+            Vector result = new Vector(size);
+
+            for (int i = 0; i < size; i++)
+                result[i] = vector[i] / diagonal[i];
+
+            return result;
+        }
     }
 }
