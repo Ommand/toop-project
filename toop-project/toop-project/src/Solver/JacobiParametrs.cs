fdc959d301c3fe80b85d097fafcd39c78756d1db
Gidroponik54 <kliminigor@gmail.com>
2016-02-23 00:21:00 +0600

    1) Added  new class Jacobiparametrs, for solver Jacobi. In fact, added only one double parametr of relaxation, and constructor 2) Added this class to compile list

---
+++ toop-project/toop-project/src/Solver/JacobiParametrs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toop_project.src.Solver
+{
+    class JacobiParametrs:ISolverParametrs
+    {
+
+        JacobiParametrs(double epsilon, int maxIterations, double optionalRelaxationParameter)
+        {
+            this.epsilon = epsilon;
+            this.maxIterations = maxIterations;
+            this.optionalRelaxationParameter = optionalRelaxationParameter;
+        }
+
+       public double epsilon { get; }
+       public int maxIterations { get; }
+       public double optionalRelaxationParameter { get; } 
+
+    }
+}
