c160ed61a813647fb478c91a1ebac981715dc6a7
Gidroponik54 <kliminigor@gmail.com>
2016-02-24 02:15:26 +0600

    GaussSeidel добавил великого

--- toop-project/toop-project/src/Solver/ISolver.cs
+++ toop-project/toop-project/src/Solver/ISolver.cs
@@ -11,7 +11,7 @@ namespace toop_project.src.Solver
 {
     public enum Type {
         Jacobi,
-        GZ,
+        GaussSeidel,
         LOS,
         MSG,
         BSG,
@@ -26,7 +26,7 @@ namespace toop_project.src.Solver
                     return new src.Solver.Jacobi();
                 case Type.MSG:
                     return new src.Solver.MSG();
-                case Type.GZ:
+                case Type.GaussSeidel:
                     return new src.Solver.GZ();
 
             }
