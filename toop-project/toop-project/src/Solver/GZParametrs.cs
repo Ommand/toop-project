072e2222c5788864dfe9f0c3792e5e9900ba3224
KozlovaAlina <Kozlova_Alina@bk.ru>
2016-02-24 01:24:05 +0600

    добавила новый класс решателя ГауссаЗейделя

---
+++ toop-project/toop-project/src/Solver/GZParametrs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toop_project.src.Solver
+{
+    class GZParametrs : ISolverParametrs
+    {
+        public GZParametrs(double epsilon, int maxIterations, double relax = 1.0)
+        {
+            this.Epsilon = epsilon;
+            this.MaxIterations = maxIterations;
+            this.Relaxation = relax;
+        }
+
+        public double Epsilon { get { return epsilon; } protected set { epsilon = value; } }
+        private double epsilon;
+        public int MaxIterations { get { return maxIterations; } protected set { maxIterations = value; } }
+        private int maxIterations;
+        public double Relaxation { get { return relaxation; } protected set { relaxation = value; } }
+        private double relaxation;
+
+    }
+}
