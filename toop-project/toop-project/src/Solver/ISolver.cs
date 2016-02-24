db6ad414cfd23a180116ea7d75bbb640e69f5934
Gidroponik54 <kliminigor@gmail.com>
2016-02-24 12:00:09 +0600

    если  яправильно понял удалить вайтпасы, то именно это я и сделал)

--- toop-project/toop-project/src/Solver/ISolver.cs
+++ toop-project/toop-project/src/Solver/ISolver.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using toop_project.src.Matrix;
+﻿using toop_project.src.Matrix;
 using toop_project.src.Vector_;
 using toop_project.src.Logging;
 
