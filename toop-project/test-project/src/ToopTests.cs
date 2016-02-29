dc190c261a3f40d76d5f3c9e7c393a0eb85e54f0
Marie <torvintardis@gmail.com>
2016-02-29 22:09:26 +0600

    initial work. create test project

---
+++ toop-project/test-project/src/ToopTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using toop_project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_project {
+	[TestClass]
+	public class ToopTests {
+		[TestMethod]
+		public void TestProfileFormat()
+		{
+		}
+	}
+}
